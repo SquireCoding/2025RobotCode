71dcd108cfa2f844c66d7f5a515117002a8ea05c
Ben D <158856931+SquireCoding@users.noreply.github.com>
2025-03-13 15:21:53 -0500

    UPdate from feather height

--- src/main/java/frc/robot/Subsystems/SElevator.java
+++ src/main/java/frc/robot/Subsystems/SElevator.java
@@ -1,6 +1,9 @@
 package frc.robot.Subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import frc.robot.Robot;
+
 import com.revrobotics.spark.SparkMax;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.Pigeon2;
@@ -10,6 +13,9 @@ import static edu.wpi.first.units.Units.Degree;
 public class SElevator {
     public static final SparkMax leftMotor = new SparkMax(31, MotorType.kBrushless);
     public static final SparkMax rightMotor = new SparkMax(32, MotorType.kBrushless);
+    public static double inc = 0.8;
+    public static double des = 0.0;
+    public static double lastDes =0.0;
     public SElevator() {
         
     }
@@ -25,4 +31,12 @@ public class SElevator {
         leftMotor.set(-0.1);
         rightMotor.set(0.1);
     }
+    public static void featherHeight(double de) {
+        if (!Robot.isWithin(leftMotor.getEncoder().getPosition(), des, 2.0)) {
+            leftMotor.set(inc);
+            rightMotor.set(-inc);
+            // if ()
+        }
+    }
+    
 }
